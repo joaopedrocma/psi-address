f50da93d4aaf67c06102c8cda37e131f22da98ff
Joao Pedro <joaopedrocma@hotmail.com>
2015-08-13 15:35:52 -0300

    Update

--- src/main/webapp/resources/app/services.js
+++ src/main/webapp/resources/app/services.js
@@ -1,53 +1,16 @@
-var service = angular.module('apiService', ['ngResource']);
+var service = angular.module('psiAddress', ['ngResource']);
+var baseUrl = 'http://localhost:8080'
+	
+service.factory('Customers', function($resource){
+	return $resource(baseUrl + '/customers/:id', { id: '@id' }, {
+		 query:  {method: 'GET', isArray: true},
+		 get:    {method: 'GET'},
+	     remove: {method: 'DELETE', params: {id: '@id'}},
+	     edit:   {method: 'PUT', params: {id: '@id'}},
+	     add:    {method: 'POST'}
+	  });
+});
 
-	//var baseUrl = 'http://177.73.233.187:9380';
-	var baseUrl = 'http://localhost:9380'
-		
-	service.factory('Addresses', function($resource){
-    	return $resource(baseUrl + '/addresses/:id', { id: '@id' }, {
-    		 query:  {method: 'GET', isArray: true},
-    		 get:    {method: 'GET'},
-    	     remove: {method: 'DELETE', params: {id: '@id'}},
-    	     edit:   {method: 'PUT', params: {id: '@id'}},
-    	     add:    {method: 'POST'}
-    	  });
-    });
+
+
 	
-	service.factory('Customers', function($resource){
-    	return $resource(baseUrl + '/customers/:id', { id: '@id' }, {
-   		 query:  {method: 'GET', isArray: true},
-   		 get:    {method: 'GET'},
-   		 remove: {method: 'DELETE', params: {id: '@id'}},
-	     edit:   {method: 'PUT', params: {id: '@id'}},
-   	     add:    {method: 'POST'}
-   	  });
-    });
-	
-	service.factory('CustomerBankAccounts', function($resource){
-    	return $resource(baseUrl + '/customerBankAccounts/:id', { id: '@id' }, {
-   		 query:  {method: 'GET', isArray: true},
-   		 get:    {method: 'GET'},
-   		 remove: {method: 'DELETE', params: {id: '@id'}},
-	     edit:   {method: 'PUT', params: {id: '@id'}},
-   	     add:    {method: 'POST'}
-   	  });
-    });
-	
-	service.factory('Tasks', function($resource){
-    	return $resource(baseUrl + '/tasks/:id', { id: '@id' }, {
-   		 query:  {method: 'GET', isArray: true},
-   		 get:    {method: 'GET'},
-   		 remove: {method: 'DELETE', params: {id: '@id'}},
-	     edit:   {method: 'PUT', params: {id: '@id'}},
-   	     add:    {method: 'POST'}
-   	  });
-    });
-	
-	
-	
-	
-	
-	
-	
-	
-		
